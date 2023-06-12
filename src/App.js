7b8001086b51befe4856ae16508bb7d34ee2402a
Gokul-96 <gokulakrishnan.m96@gmail.com>
2023-06-12 15:08:07 +0530

    react app deploy

--- src/App.js
+++ src/App.js
@@ -1,25 +1,45 @@
-import logo from './logo.svg';
-import './App.css';
+// function App () {
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+//   const now =new Date()
+
+//   const a = 20;
+//   const b = 50;
+
+
+// console.log(now, a+b);
+//   return (
+//    <div>Let's Do
+//     <p>{now.toString()}</p>
+//     <p>{a} plus {b} is {a+b}</p>
+//    </div>
+//   );
+// }
+
+
+// export default App;
+
+import React from 'react'
+
+function Hello(props){
+  //console.log(props);
+  return(
+    <div>
+      <h1>Hello{props.name}{props.age}</h1>
+      
     </div>
-  );
+  )
 }
 
-export default App;
+function App() {
+  const age =18;
+  return (
+    <div>
+      <h1>welcome</h1>
+      <Hello name='gokul' age={10+30}/>
+      <Hello name="Gok" age={age}/>
+      <Hello />
+    </div>
+  )
+}
+
+export default App
